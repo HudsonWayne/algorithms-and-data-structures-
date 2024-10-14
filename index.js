ec201902aa537815544b90ad930b5b136519d221
HudsonWayne <waynewaynebenhura@gmail.com>
2024-10-14 13:43:33 +0200

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function isPrime(n){
     if (n<2){
         return false;
     }
+    for(let i=2; i<n; 1++)
+        if(n%i === 0){
+            return false;
+        }
 }
 console.log(isPrime(1))
 console.log(isPrime(5))
