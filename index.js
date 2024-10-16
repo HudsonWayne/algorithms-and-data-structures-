8d3494ac2941e7b6bf18a1fd66e04f62cf61ce2f
HudsonWayne <waynewaynebenhura@gmail.com>
2024-10-16 11:20:00 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-function linearSearch(arr, target) {
-    for (let i = 0; i < arr.length; i++) { 
-        if (arr[i] === target) {
-            return i; 
-        }
-    }
-    return -1; 
-}
-console.log(linearSearch([-5, 2, 10, 4, 6], 10)); // Output: 2
-console.log(linearSearch([-5, 2, 10, 4, 6], 6)); // Output: 4
-console.log(linearSearch([-5, 2, 10, 4, 6], 20)); // Output: -1
+let ten = 10;
+console.log(ten);
